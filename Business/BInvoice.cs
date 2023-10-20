cbee39e566fe8ae39d37a55cbbac70f7e482fb84
Alumno <Alumno@LAB1504-29>
2023-10-19 21:58:44 -0500

    Agregar archivos de proyecto.

---
+++ Business/BInvoice.cs
@@ -0,0 +1,33 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Business
+{
+    public class BInvoice
+    {
+        public List<Invoice> GetByDate(DateTime date)
+        {
+
+            DInvoice data = new DInvoice();
+            var invoices = data.Get();
+            var result = invoices.Where(x => x.date == date).ToList();
+            //lambda          
+
+            return result;
+        }
+        public List<Invoice> Get()
+        {
+
+
+            DInvoice data = new DInvoice();
+            var invoices = data.Get();
+
+            return invoices;
+        }
+    }
+}
---
+++ Lab07/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace Lab07
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DateTime invoiceDate;
+
+            if (DateTime.TryParse(txtDate.Text, out invoiceDate))
+            {
+                BInvoice bInvoice = new BInvoice();
+                List<Invoice> invoices = bInvoice.GetByDate(invoiceDate);
+                dataGrid.ItemsSource = invoices;
+            }
+            else
+            {
+                MessageBox.Show("Formato de fecha incorrecto");
+            }
+        }
+
+    }
+}
